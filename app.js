3f458282c3796a44b1f7b3646dd6fb58b773bdd5
Hope Kalitera <hopekalitera@gmail.com>
2023-09-30 00:38:12 +0200

    featL mysql configs

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 const express = require('express');
+const mysql = require('mysql2');
 
 const app = express();
+app.use(express.static("public"))
+app.use(express.urlencoded({ extended: true }))
+
+const config = require('./config');
+const connection = mysql.createPool(config.database);
 
 app.set("view engine", "ejs")
 app.use(express.static("public"))
