1340a1b8c0da3b3368aa219df69c1927c7b4c71d
Hope Kalitera <hopekalitera@gmail.com>
2023-10-09 04:42:59 +0200

    feat: members view api handler

---
+++ api/membersView.js
@@ -0,0 +1,29 @@
+const memberViewServ = require('../services/membersView');
+
+const genderconst = (req, res, connection, callback) => {
+
+    memberViewServ.teamGenders(req, res, connection, (error, data) => {
+        if (error) {
+            console.log(error);
+            res.status(500).send('Error retrieving data');
+        } else {
+            console.log(data);
+        }
+    });
+};
+const teamStuff = (req, res, connection, callback) => {
+
+    memberViewServ.teamMemberNumber(req, res, connection, (error, data) => {
+        if (error) {
+            console.log(error);
+            res.status(500).send('Error retrieving data');
+        } else {
+            console.log(data);
+        }
+    });
+};
+
+module.exports = {
+    genderconst,
+    teamStuff
+}
