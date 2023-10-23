f46e6ed619b885c78e6f7c28af61b693d2a93776
Hope Kalitera <hopekalitera@gmail.com>
2023-10-24 01:36:08 +0200

    feat: add delete filter when selecting

--- services/membersView.js
+++ services/membersView.js
@@ -1,5 +1,5 @@
 const teamMemberNumber = (req, res, connection, callback) => {
-    connection.query(`SELECT t.name AS team_name, COUNT(tm.member_id) AS total_members, SUM(CASE WHEN tm.gender = 'Male' THEN 1 ELSE 0 END) AS male_count, SUM(CASE WHEN tm.gender = 'Female' THEN 1 ELSE 0 END) AS female_count FROM team t LEFT JOIN teamMembers tm ON t.id = tm.team_id GROUP BY t.id`, (error, results) => {
+    connection.query(`SELECT t.name AS team_name, COUNT(tm.member_id) AS total_members, SUM(CASE WHEN tm.gender = 'Male' THEN 1 ELSE 0 END) AS male_count, SUM(CASE WHEN tm.gender = 'Female' THEN 1 ELSE 0 END) AS female_count FROM team t LEFT JOIN teamMembers tm ON t.id = tm.team_id AND tm.deleted_at IS NULL WHERE t.deleted_at IS NULL GROUP BY t.id, t.name;`, (error, results) => {
         if (error) {
           return callback(error, null);
         }
