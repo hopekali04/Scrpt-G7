01139e6c317db4384aa9027666f2be6121f6483d
Hope Kalitera <hopekalitera@gmail.com>
2023-10-24 01:35:48 +0200

    fix: remove bottlenecks

--- api/cropViews.js
+++ api/cropViews.js
@@ -2,91 +2,18 @@ const cropService = require('../services/cropViews');
 
 const getTotlcropView = (req, res, connection, callback) => {
 
-    let highestCrop = null;
-    let lowestCrop = null;
-    let totalCrops = null;
-    let totalCropTypes = null;
-    let countCropSeasons = null;
     let distinctCrop= null;
 
-    cropService.totalCrops(req, res, connection, (error, data) => {
-        if (error) {
-            console.log(error);
-            res.status(500).send('Error retrieving crop');
-        } else {
-            totalCrops = data;
-
-            // Check if all data is available before logging
-            if (highestCrop !== null && lowestCrop !== null && totalCrops !== null && totalCropTypes !== null && countCropSeasons !== null && distinctCrop !== null) {
-                //console.log(highestCrop, lowestCrop, totalCrops, totalCropTypes, countCropSeasons);
-            }
-        }
-    });
-
-    cropService.totalCropTypes(req, res, connection, (error, data) => {
-        if (error) {
-            console.log(error);
-            res.status(500).send('Error retrieving crop');
-        } else {
-            totalCropTypes = data;
-
-            // Check if all data is available before logging
-            if (highestCrop !== null && lowestCrop !== null && totalCrops !== null && totalCropTypes !== null && countCropSeasons !== null && distinctCrop !== null) {
-                //console.log(highestCrop, lowestCrop, totalCrops, totalCropTypes, countCropSeasons);
-            }
-        }
-    });
-
-    cropService.countCropSeasons(req, res, connection, (error, data) => {
-        if (error) {
-            console.log(error);
-            res.status(500).send('Error retrieving crop');
-        } else {
-            countCropSeasons = data;
-
-            // Check if all data is available before logging
-            if (highestCrop !== null && lowestCrop !== null && totalCrops !== null && totalCropTypes !== null && countCropSeasons !== null && distinctCrop !== null) {
-                //console.log(highestCrop, lowestCrop, totalCrops, totalCropTypes, countCropSeasons);
-            }
-        }
-    });
-
-    cropService.highestCrop(req, res, connection, (error, data) => {
-        if (error) {
-            console.log(error);
-            res.status(500).send('Error retrieving crop');
-        } else {
-            highestCrop = data;
-
-            // Check if all data is available before logging
-            if (highestCrop !== null && lowestCrop !== null && totalCrops !== null && totalCropTypes !== null && countCropSeasons !== null && distinctCrop !== null) {
-                //console.log(highestCrop, lowestCrop, totalCrops, totalCropTypes, countCropSeasons);
-            }
-        }
-    });
-
-    cropService.lowestCrop(req, res, connection, (error, data) => {
-        if (error) {
-            console.log(error);
-            res.status(500).send('Error retrieving crop');
-        } else {
-            lowestCrop = data;
-
-            // Check if all data is available before logging
-            if (highestCrop !== null && lowestCrop !== null && totalCrops !== null && totalCropTypes !== null && countCropSeasons !== null && distinctCrop !== null) {
-                //console.log(highestCrop, lowestCrop, totalCrops, totalCropTypes, countCropSeasons);
-            }
-        }
-    });
     cropService.distinctCrop(req, res, connection, (error, data) => {
         if (error) {
             console.log(error);
             res.status(500).send('Error retrieving crop');
         } else {
+            console.log(data);
             distinctCrop = data;
 
             // Check if all data is available before logging
-            if (highestCrop !== null && lowestCrop !== null && totalCrops !== null && totalCropTypes !== null && countCropSeasons !== null && distinctCrop !== null && distinctCrop !== null) {
+            if (distinctCrop !== null) {
                 console.log(distinctCrop);
                 return callback(null, distinctCrop);
             }
